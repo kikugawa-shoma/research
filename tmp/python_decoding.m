1c85ff4525072dabdf8275ae4098cffabc9122b3
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2020-04-25 00:14:34 +0900

    [add]

--- tmp/python_decoding.m
+++ tmp/python_decoding.m
@@ -1,16 +1,16 @@
-cd ../Python/BrainDL
+cd ../Python/Brain_DL
 fileinfos=dir("res*.mat")
-file_num=size(filesinfos,1)
+file_num=size(fileinfos,1)
 cd ../../tmp_results/
 iter=10
 acc=zeros(file_num,iter)
+
 for i=1:file_num
 
     load(fileinfos(i).name)
     for j=1:iter
         L=label(label==3|label==4);
         data=X(label==3|label==4,:);
-
         ind_tr=sort(randsample(size(L,1), round(size(L,1)*0.8)));
         ind_test=setdiff(1:size(L,1), ind_tr);
 
@@ -22,5 +22,7 @@ for i=1:file_num
         model=fitcsvm(data_tr,label_tr);
         pred=predict(model,data_test);
         acc(i,j)=(sum(pred==label_test)/size(pred,1))*100
+    end
+end
 
 save("python_decoding_results.mat","fileinfos","acc")

2514d677a5a79e0a3805a55d252e74f632a8175f
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2020-11-11 16:41:23 +0900

    [add]p-value pagerank classifier

--- tmp/pagerank_classify.py
+++ tmp/pagerank_classify.py
@@ -2,6 +2,7 @@ from sklearn.svm import SVC
 import numpy as np
 from svm_raw_nearests_decoding import PageRanks as PR
 from collections import defaultdict
+import copy
 
 class PagerankDecoder(SVC):
     def __init__(self,kernel="rbf"):
@@ -19,14 +20,46 @@ class PagerankDecoder(SVC):
         super().fit(self.train_x,self.train_y)
         return int(super().predict(self.test_x.reshape(1,-1)))
 
+def t_test_classifier(pagerank,label,test_ind):
+    from scipy import stats
+    N = len(label)
+    train_ind = [True]*N
+    train_ind[test_ind] = False
+    pagerank = np.array(pagerank)
+    label = np.array(label)
+
+    train_x = pagerank[train_ind]
+    train_y = label[train_ind]
+    c_pagerank = [[] for _ in range(len(set(label)))]
+    for i in range(len(train_y)):
+        c_pagerank[train_y[i]].append(train_x[i])
+    sum_ps = []
+    for i in range(2):
+        cc_pagerank = copy.deepcopy(c_pagerank)
+        cc_pagerank[i].append(pagerank[test_ind])
+        ts,ps = stats.ttest_ind(cc_pagerank[0],cc_pagerank[1])
+        sum_ps.append(sum(ps))
+    return sum_ps.index(min(sum_ps))
+    
+
 if __name__ == "__main__":
     with open(r"results\confusion_mat_classified_label.txt") as f:
         label = list(map(int,f.readline().split()))
+    """
     predicted_label = []
     for i in [0]:
         model = PagerankDecoder()
         predicted_label.append(model.fit_predict(PR().pr,label,i))
     print(predicted_label)
+    """
+
+    predicted_label = []
+    for i in range(len(label)):
+        predicted_label.append(t_test_classifier(PR().pr,label,i))
+    label = np.array(label)
+    predicted_label = np.array(predicted_label)
+    print(sum(label == predicted_label)/51)
+    
     
 
 

fe881dcf7cc881a8120b8da5aea6594c5d822590
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2020-11-12 18:28:13 +0900

    :put_litter_in_its_place:

--- tmp/pagerank_classify.py
+++ tmp/pagerank_classify.py
@@ -20,26 +20,6 @@ class PagerankDecoder(SVC):
         super().fit(self.train_x,self.train_y)
         return int(super().predict(self.test_x.reshape(1,-1)))
 
-def t_test_classifier(pagerank,label,test_ind):
-    from scipy import stats
-    N = len(label)
-    train_ind = [True]*N
-    train_ind[test_ind] = False
-    pagerank = np.array(pagerank)
-    label = np.array(label)
-
-    train_x = pagerank[train_ind]
-    train_y = label[train_ind]
-    c_pagerank = [[] for _ in range(len(set(label)))]
-    for i in range(len(train_y)):
-        c_pagerank[train_y[i]].append(train_x[i])
-    sum_ps = []
-    for i in range(2):
-        cc_pagerank = copy.deepcopy(c_pagerank)
-        cc_pagerank[i].append(pagerank[test_ind])
-        ts,ps = stats.ttest_ind(cc_pagerank[0],cc_pagerank[1])
-        sum_ps.append(sum(ps))
-    return sum_ps.index(min(sum_ps))
     
 
 if __name__ == "__main__":
@@ -56,14 +36,6 @@ if __name__ == "__main__":
     label = np.array(label)
     print(sum(label == predicted_label)/51)
 
-    """
-    predicted_label = []
-    for i in range(len(label)):
-        predicted_label.append(t_test_classifier(PR().pr,label,i))
-    label = np.array(label)
-    predicted_label = np.array(predicted_label)
-    print(sum(label == predicted_label)/51)
-    """
     
     
 

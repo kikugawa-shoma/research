a20bf93054ec91bce0c089d1cb2c35324464aaaf
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2020-11-26 16:34:19 +0900

    label given predict

--- tmp/pagerank_classify.py
+++ tmp/pagerank_classify.py
@@ -15,22 +15,35 @@ class PagerankDecoder(SVC):
         train_ind = [True if c != None else False for c in Y]
         train_x = X[train_ind,:]
         train_y = np.array(Y[train_ind],dtype=int)
-        print(train_y)
         super().fit(train_x,train_y)
     def predict(self,x):
         return super().predict(x)
 
 
 if __name__ == "__main__":
-    for target in range(1):
-        label = cf.ConfusionMatrix().community_detection_without(target)
+    accuracy = 0
+    for target in range(51):
+        #label = cf.ConfusionMatrix().community_detection_without(target)
+
+        # This is the comment option
+        with open(r"results\confusion_mat_classified_label.txt") as f:
+            all_label = list(map(int,f.readline().split()))
+        
+        label = copy.copy(all_label)
+        label[target] = None
+
         pagerank = PR.PageRanks()
         
         sig_ps_ind = PR.PageRanks().ttest_significant_ind(target = target)
 
         model = PagerankDecoder()
         model.fit(pagerank.pr[:,sig_ps_ind],label)
-        print(target,model.predict(pagerank.pr[target,sig_ps_ind].reshape(1,-1)))
+        pred = model.predict(pagerank.pr[target,sig_ps_ind].reshape(1,-1))
+        print(target,all_label[target],pred)
+        if label[target] == pred[0]:
+            accuracy += 1
+    accuracy = accuracy/51
+    print(accuracy)
 
     """
     predicted_label = []

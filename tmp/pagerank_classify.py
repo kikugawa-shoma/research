4a6032466d2305817cc1fbfcce1035f1292f3c31
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2020-12-17 16:29:16 +0900

    tmp

--- tmp/pagerank_classify.py
+++ tmp/pagerank_classify.py
@@ -7,21 +7,6 @@ import community_find as cf
 import matplotlib.pyplot as plt
 import scipy
 
-class PagerankDecoder(SVC):
-    def __init__(self,C,gamma,kernel="rbf",class_weight=None):
-        super().__init__(C=C,gamma=gamma,kernel=kernel,class_weight=class_weight)
-    def fit(self,X,Y):
-        X = np.array(X)
-        Y = np.array(Y)
-        self.N = len(Y)
-        train_ind = [True if c != None else False for c in Y]
-        train_x = X[train_ind,:]
-        train_y = np.array(Y[train_ind],dtype=int)
-        super().fit(train_x,train_y)
-    def predict(self,x):
-        return super().predict(x)
-
-
 if __name__ == "__main__":
     accuracy = 0
     sig_img = []
@@ -38,39 +23,29 @@ if __name__ == "__main__":
     with open("results\\confusion_mat_classified_label.txt") as f:
         all_label = list(map(int,f.readline().split()))
     """
+    all_label = np.array(all_label)
     for target in range(51):
-        # label = cf.ConfusionMatrix().community_detection_without(target)
-
-        # This is the comment option
-        label = copy.copy(all_label)
-        label[target] = None
 
         pagerank = PR.PageRanks(weighted="w")
 
-        sig_ps_ind = pagerank.ttest_significant_ind(target = target,alpha=0.05,sampling=None)
-        sig_img.append(sig_ps_ind)
-        # sig_ps_ind1 = np.load(r"results\e_num5\sig_ps.npy")
+        train_ind = np.array([True if i!=target else False for i in range(51)])
 
-        model = PagerankDecoder(C=1,gamma="auto",class_weight="balanced")
-        
-        model.fit(pagerank.pr[:,sig_ps_ind],label)
-        pred = model.predict(pagerank.pr[target,sig_ps_ind].reshape(1,-1))
+        sig_ps_ind = pagerank.ttest_significant_ind(target=target)
+
+        d = {1:sum(all_label==0),0:sum(all_label==1)}
+
+        model = SVC(C=1,gamma="auto",kernel="rbf",class_weight="balanced")
+        model.fit(pagerank.pr[train_ind,:][:,sig_ps_ind],all_label[train_ind])
+
+        for i in range(51):
+            print(model.predict(pagerank.pr[i,][sig_ps_ind].reshape(1,-1))[0],end=" ")
+        print("")
+
+        pred = model.predict(pagerank.pr[target,:][sig_ps_ind].reshape(1,-1))
         predicted_label.append(pred[0])
-        print(target,all_label[target],pred,sum(sig_ps_ind))
         if all_label[target] == pred[0]:
             accuracy += 1
     accuracy = accuracy/51
     print(accuracy)
-    plt.matshow(sig_img,aspect=20)
-    plt.show()
-
-
-    """
-    predicted_label = []
-    for i in range(len(label)):
-        model = PagerankDecoder()
-        predicted_label.append(model.fit_predict(PR().pr[:,sig_ps],label,i))
-    predicted_label = np.array(predicted_label)
-    label = np.array(label)
-    print(sum(label == predicted_label)/51)
-    """
+    print(np.array(predicted_label))
+    print(all_label)

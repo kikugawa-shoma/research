aa68e4e03fa89074d4ddb31e43f27c47c7d04b85
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2021-01-14 13:51:57 +0900

    tmp

--- tmp/pagerank_classify.py
+++ tmp/pagerank_classify.py
@@ -34,11 +34,11 @@ if __name__ == "__main__":
         label = copy.copy(all_label)
         label[target] = None
 
-        feature_value = PR.PageRanks(weighted=True)
+        feature_value = PR.PageRanks(weighted=False)
 
         sig_ps_ind = feature_value.ttest_significant_ind(target = target,alpha=0.05,sampling=None,sample_diff=30)
         sig_img.append(sig_ps_ind)
-        sig_ps_ind1 = np.load(r"results\e_num5\sig_ps.npy")
+        sig_ps_ind = np.load(r"results\e_num5\sig_ps.npy")
 
         model = PagerankDecoder(C=1,gamma="scale",class_weight="balanced")
         

61b81788a3b7eb48580eb574ec8166026a671446
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2021-01-27 15:12:07 +0900

    tmp

--- tmp/pagerank_classify.py
+++ tmp/pagerank_classify.py
@@ -1,69 +1,48 @@
-from sklearn.svm import SVC
+from sklearn.svm import OneClassSVM
 import numpy as np
 import PageRank as PR
 from collections import defaultdict
 import copy
 import community_find as cf
 import matplotlib.pyplot as plt
-
-class PagerankDecoder(SVC):
-    def __init__(self,C,gamma,kernel="rbf",class_weight=None):
-        super().__init__(C=C,gamma=gamma,kernel=kernel,class_weight=class_weight)
-    def fit(self,X,Y):
-        X = np.array(X)
-        Y = np.array(Y)
-        self.N = len(Y)
-        train_ind = [True if c != None else False for c in Y]
-        train_x = X[train_ind,:]
-        train_y = np.array(Y[train_ind],dtype=int)
-        super().fit(train_x,train_y)
-    def predict(self,x):
-        return super().predict(x)
+from matplotlib import pyplot as plt
 
 
-if __name__ == "__main__":
-    accuracy = 0
-    sig_img = []
-    predicted_label = []
-
-    pagerank_kind = "p" # "p" or "w" or "wt"
-    for target in range(51):
-        # label = cf.ConfusionMatrix().community_detection_without(target)
-
-        # This is the comment option
-        with open(r"results\confusion_mat_classified_label.txt") as f:
-            all_label = list(map(int,f.readline().split()))
-        label = copy.copy(all_label)
-        label[target] = None
-
-        feature_value = PR.PageRanks(weighted=pagerank_kind)
-
-        sig_ps_ind = feature_value.ttest_significant_ind(target = target,alpha=0.05,sampling=None,sample_diff=30)
-        sig_img.append(sig_ps_ind)
-        ps = np.load("results//e_num5//ps_"+pagerank_kind+".npy")
-        sig_ps_ind = ps<0.05
-
-        model = PagerankDecoder(C=1,gamma="scale",class_weight="balanced")
-        
-        model.fit(np.delete(feature_value.pr[:,sig_ps_ind],14,0),np.delete(label,14,0))
-        pred = model.predict(feature_value.pr[target,sig_ps_ind].reshape(1,-1))
-        predicted_label.append(pred[0])
-        print(target,all_label[target],pred,sum(sig_ps_ind))
-        if all_label[target] == pred[0]:
-            accuracy += 1
-    accuracy = accuracy/51
-    print(accuracy)
-    plt.matshow(sig_img,aspect=20)
-    plt.show()
-    np.save("results\\pagerank_classified_label_" + pagerank_kind + ".npy",predicted_label)
 
 
-    """
-    predicted_label = []
-    for i in range(len(label)):
-        model = PagerankDecoder()
-        predicted_label.append(model.fit_predict(PR().pr[:,sig_ps],label,i))
-    predicted_label = np.array(predicted_label)
-    label = np.array(label)
-    print(sum(label == predicted_label)/51)
-    """
+pagerank_kind = "wt" # "p" or "w" or "wt"
+with open(r"results\confusion_mat_classified_label.txt") as f:
+    all_label = list(map(int,f.readline().split()))
+
+feature_value = PR.PageRanks(weighted=pagerank_kind)
+
+
+positive = sum([l == 0 for l in all_label])
+negative = sum([l != 0 for l in all_label])
+
+TPRs = []
+TNRs = []
+for i in range(1,11):
+    model = OneClassSVM(nu=i/10)
+    pred = model.fit_predict(feature_value.pr)
+    pred = [0 if p == 1 else 1 for p in pred]
+    print(pred)
+    TPR = 0
+    TNR = 0
+    for j in range(len(pred)):
+        if all_label[j] == 0 and pred[j] == 0:
+            TPR += 1
+        if all_label[j] == 1 and pred[j] == 1:
+            TNR += 1
+    TPR /= positive
+    TNR /= negative
+    TPRs.append(TPR)
+    TNRs.append(TNR)
+
+plt.plot(TNRs,TPRs)
+plt.plot([0,1],[1,0],c="red")
+plt.show()
+
+
+
+

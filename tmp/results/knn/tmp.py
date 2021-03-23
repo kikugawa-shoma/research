95fb8a5736a8462d1c43f07a76771acc891e8d0f
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2021-03-23 14:47:36 +0900

    tmp

--- tmp/results/knn/tmp.py
+++ tmp/results/knn/tmp.py
@@ -3,10 +3,14 @@ from matplotlib import pyplot as plt
 
 def make_plot(filename):
     data = np.load(filename)
+    fig = plt.figure()
     plt.plot([i for i in range(len(data))],data)
+    plt.title(filename)
     plt.show()
+    fig.savefig("C:\\Users\\ktmks\\OneDrive\\画像\\tmp\\"+filename+".png")
 
-filenames = ["dmap_with_t-test.npy",
+filenames = [
+"dmap_with_t-test.npy",
 "dmap_without_t-test.npy",
 "pagerank_with_t-test.npy",
 "pagerank_without_t-test.npy",
@@ -14,7 +18,3 @@ filenames = ["dmap_with_t-test.npy",
 
 for i in range(len(filenames)):
     make_plot(filenames[i])
-
-
-
-
